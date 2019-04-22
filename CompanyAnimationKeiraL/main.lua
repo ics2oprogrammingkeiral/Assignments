b7a347dbb44d03f3296336f61e3ae1655e522dba
s269682 <keira.lawson@ocsbstudent.ca>
2019-04-22 18:25:37 -0400

    company animation changes

--- CompanyAnimationKeiraL/main.lua
+++ CompanyAnimationKeiraL/main.lua
@@ -10,14 +10,79 @@ display.setStatusBar(display.HiddenStatusBar)
 -- set the background colour
 display.setDefault("background", 0.96078, 0.96078, 0.96078)
 
--- load physics
-local physics = require("physics")
-
--- start physics
-physics.start()
-
+---------------------------------------------------------------
+-- VARIABLES -- 
+---------------------------------------------------------------
+scrollSpeed = 10
 
 ---------------------------------------------------------------
 -- OBJECTS -- 
 ---------------------------------------------------------------
--- border
+-- create the plate
+local plate = display.newImage("Images/plate.png", 0, 0)
+	plate.x = display.contentWidth/2
+	plate.y = display.contentHeight/-2
+
+-- create the fork
+local fork = display.newImage("Images/fork.png", 0, 0)
+	fork.x = display.contentWidth/2
+	fork.y = display.contentHeight/-2
+
+-- create the spoon
+local spoon = display.newImage("Images/spoon.png", 0, 0)
+	spoon.x = display.contentWidth/2
+	spoon.y = display.contentHeight/-2
+-- create the slime
+local slime = display.newImage("Images/slime.png", 0, 0)
+	slime.x = display.contentWidth/2
+	slime.y = display.contentHeight/-2
+
+---------------------------------------------------------------
+-- FUNCTIONS -- 
+---------------------------------------------------------------
+-- move the plate to the starting poisition
+local function MovePlate( event )
+	-- add the scroll speed to the y-value
+	plate.y = plate.y + scrollSpeed
+end
+
+-- move the fork to the starting poisition
+local function MoveFork( event )
+	-- add the scroll speed to the y-value
+	fork.y = fork.y + scrollSpeed
+end
+
+-- move the spoon to the starting poisition
+local function MoveSpoon( event )
+	-- add the scroll speed to the y-value
+	spoon.y = spoon.y + scrollSpeed
+end
+
+-- move the slime to the starting poisition
+local function MoveSlime( event )
+	-- add the scroll speed to the y-value
+	slime.y = slime.y + scrollSpeed
+end
+
+---------------------------------------------------------------
+-- EVENT LISTENERS -- 
+---------------------------------------------------------------
+Runtime:addEventListener("EnterFrame", MovePlate, 300)
+Runtime:addEventListener("EnterFrame", MoveSlime)
+Runtime:addEventListener("EnterFrame", MoveFork)
+Runtime:addEventListener("EnterFrame", MoveSpoon)
+---------------------------------------------------------------
+-- DELAYS -- 
+---------------------------------------------------------------
+timer.performWithDelay(0, MovePlate)
+timer.performWithDelay(0, MoveSlime)
+timer.performWithDelay(200, MoveFork)
+timer.performWithDelay(200, MoveSpoon)
+
+---------------------------------------------------------------
+-- START FUNCTIONS -- 
+---------------------------------------------------------------
+MovePlate()
+MoveSlime()
+MoveFork()
+MoveSpoon()
