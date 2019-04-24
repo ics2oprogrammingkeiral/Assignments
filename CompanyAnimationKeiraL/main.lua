7af0030a012c320affb5779726732aa93dae6abb
Keira Lawson <47783086+s269682@users.noreply.github.com>
2019-04-24 09:43:40 -0400

    fixed picture poisitioning

--- CompanyAnimationKeiraL/main.lua
+++ CompanyAnimationKeiraL/main.lua
@@ -8,7 +8,7 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 -- set the background colour
-display.setDefault("background", 0.96078, 0.96078, 0.96078)
+display.setDefault("background", 1, 1, 1)
 
 ---------------------------------------------------------------
 -- VARIABLES -- 
@@ -19,23 +19,25 @@ scrollSpeed = 10
 -- OBJECTS -- 
 ---------------------------------------------------------------
 -- create the plate
-local plate = display.newImage("Images/plate.png", 0, 0)
+	
+local plate = display.newImageRect("Images/plate.png", 600, 400)
 	plate.x = display.contentWidth/2
 	plate.y = display.contentHeight/-2
 
+-- create the slime
+local slime = display.newImageRect("Images/slime.png", 600, 400)
+	slime.x = display.contentWidth/2
+	slime.y = display.contentHeight/-3
+
 -- create the fork
-local fork = display.newImage("Images/fork.png", 0, 0)
-	fork.x = display.contentWidth/2
+local fork = display.newImageRect("Images/fork.png", 150, 450)
+	fork.x = display.contentWidth/8
 	fork.y = display.contentHeight/-2
 
 -- create the spoon
-local spoon = display.newImage("Images/spoon.png", 0, 0)
-	spoon.x = display.contentWidth/2
+local spoon = display.newImageRect("Images/spoon.png", 150, 450)
+	spoon.x = 880
 	spoon.y = display.contentHeight/-2
--- create the slime
-local slime = display.newImage("Images/slime.png", 0, 0)
-	slime.x = display.contentWidth/2
-	slime.y = display.contentHeight/-2
 
 ---------------------------------------------------------------
 -- FUNCTIONS -- 
@@ -44,6 +46,12 @@ local slime = display.newImage("Images/slime.png", 0, 0)
 local function MovePlate( event )
 	-- add the scroll speed to the y-value
 	plate.y = plate.y + scrollSpeed
+end
+
+-- move the slime to the starting poisition
+local function MoveSlime( event )
+	-- add the scroll speed to the y-value
+	slime.y = slime.y + scrollSpeed
 end
 
 -- move the fork to the starting poisition
@@ -58,31 +66,11 @@ local function MoveSpoon( event )
 	spoon.y = spoon.y + scrollSpeed
 end
 
--- move the slime to the starting poisition
-local function MoveSlime( event )
-	-- add the scroll speed to the y-value
-	slime.y = slime.y + scrollSpeed
-end
-
 ---------------------------------------------------------------
 -- EVENT LISTENERS -- 
 ---------------------------------------------------------------
-Runtime:addEventListener("EnterFrame", MovePlate, 300)
-Runtime:addEventListener("EnterFrame", MoveSlime)
-Runtime:addEventListener("EnterFrame", MoveFork)
-Runtime:addEventListener("EnterFrame", MoveSpoon)
----------------------------------------------------------------
--- DELAYS -- 
----------------------------------------------------------------
-timer.performWithDelay(0, MovePlate)
-timer.performWithDelay(0, MoveSlime)
-timer.performWithDelay(200, MoveFork)
-timer.performWithDelay(200, MoveSpoon)
+Runtime:addEventListener("enterFrame", MovePlate)
+Runtime:addEventListener("enterFrame", MoveSlime)
+Runtime:addEventListener("enterFrame", MoveFork)
+Runtime:addEventListener("enterFrame", MoveSpoon)
 
----------------------------------------------------------------
--- START FUNCTIONS -- 
----------------------------------------------------------------
-MovePlate()
-MoveSlime()
-MoveFork()
-MoveSpoon()
